8a10fb0f8876b8a069f4f372f0cc767b9758a050
lxc <173957968@qq.com>
2017-05-10 00:22:17 +0800

    项目创建

---
+++ captcha/src/main/java/hcy/captcha/App.java
@@ -0,0 +1,13 @@
+package hcy.captcha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
